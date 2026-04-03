14e3ad9a6b807a9352b6f21545c16f74ea28888b
Eduardo Gutierrez <eduardo_gd@hotmail.com>
2026-04-03 07:55:36 -0600

    first email

---
+++ src/test/java/egd/fmre/bulkemail/BulkEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package egd.fmre.bulkemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
